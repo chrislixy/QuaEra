1e819d5ed09b6ae11e67c1f543fc8f62926025d8
Chris Li <chrislixystat@gmail.com>
2016-04-11 02:22:45 -0400

    only use last viewed info

---
+++ NaiveModel.R
@@ -0,0 +1,8 @@
+sum(purchase$A==lastview$A & purchase$B==lastview$B & 
+    purchase$C==lastview$C & purchase$D==lastview$D & 
+    purchase$E==lastview$E & purchase$F==lastview$F & 
+    purchase$G==lastview$G )/dim(purchase)[1]
+
+sum(purchase$A==lastview$A & purchase$B==lastview$B & 
+      purchase$C==lastview$C & purchase$D==lastview$D & 
+      purchase$E==lastview$E & purchase$F==lastview$F)/dim(purchase)[1]
